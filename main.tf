7b1b544c070e68c3f998512bf5d7162509b9af08
Kamil Jeziorny <kamij98@gmail.com>
2025-03-19 15:54:15 +0100

    egress config for ec2

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ resource "aws_security_group" "test_server_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Restrict this for better security
   }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "test_server" {
